69ac2bf9f2a971094bacab2b7e438db9bd2bb440
Jonatan Åkerlind <jonatan@akerlind.nu>
2014-10-17 16:56:46 +0200

    DCM compensator. Stable with chosen values.

--- doc/calculations/roomunit_power_optoisolator_loop_compensation.m
+++ doc/calculations/roomunit_power_optoisolator_loop_compensation.m
@@ -1,3 +1,5 @@
+%% chosen component values from E48 series if possible
+
 close all
 clc
 clear
@@ -13,23 +15,23 @@ Ioutmax=0.25
 Vr=Voutnom*0.02
 
 % Controller bias power
-Pbiasmax=12*1.2e-3
+Pbiasmax=12*1.2e-3;
 
 % DC output power
-Pdcoutmax=Voutnom*Ioutmax
+Pdcoutmax=Voutnom*Ioutmax;
 % Flyback max output power
-Poutmax=Pdcoutmax+Pbiasmax
-Rlmin=Voutnom/Ioutmax
+Poutmax=Pdcoutmax+Pbiasmax;
+Rlmin=Voutnom/Ioutmax;
 % Min output current/power = 0 (=> DCM)
 
-Mvdcmin=Voutnom/Vinmax
-Mvdcmax=Voutnom/Vinmin
+Mvdcmin=Voutnom/Vinmax;
+Mvdcmax=Voutnom/Vinmin;
 % target efficiency
 etaFB=0.85
 % assumed duty cycle at CCM/DCM boundary
 Dbmax=0.3
 
-Npsmin=etaFB*Dbmax/((1-Dbmax)*Mvdcmax)
+Npsmin=etaFB*Dbmax/((1-Dbmax)*Mvdcmax);
 % Chosen primary:secondary winding relationship
 Nps=5.5
 
@@ -37,7 +39,7 @@ Nps=5.5
 freqnom=2e5
 
 % Maximum magnetizing inductance for DCM
-Lmmax=Nps^2*Rlmin*(1-Dbmax)^2/(2*freqnom)
+Lmmax=Nps^2*Rlmin*(1-Dbmax)^2/(2*freqnom);
 % matching PoE trafo: würth electronics 750310019
 % Chosen input winding inductance
 Lp=310e-6
@@ -55,90 +57,92 @@ Vsmmax=Vinmax+Nps*Voutnom
 
 % Voltage spike at transistor turnoff
 % trafo leakage inductance (max)
-Ll=5e-6
+Ll=5e-6;
 % trafo primary winding resistance (max)
-Rp=1.6
+Rp=1.6;
 % trafo secondary winding resistance (max)
-Rs=0.045
+Rs=0.045;
 % trafo stray input capacitance (estimated)
-Cp=10e-12
+Cp=10e-12;
 
 % transistor output capacitonce (Vgs=0, Vds=25)
-Coss=100e-12
+Coss=100e-12;
 
-omegao=1/sqrt(Ll*(Coss+Cp))
+omegao=1/sqrt(Ll*(Coss+Cp));
 freqring=omegao/(2*pi)
 % characteristic impedance of ringing (input) circuit
-Zo=sqrt(Ll/(Coss+Cp))
+Zo=sqrt(Ll/(Coss+Cp));
 Vsmmax=Vinmax+Nps*Voutnom+Ismmax*Zo
 
 % ringing power loss
-Pring=freqnom*Ll*Ismmax^2/2
+Pring=freqnom*Ll*Ismmax^2/2;
 
 % Output filter capacitors
 % assuming rc=20mOhm
-rc=20e-3
-Vrcpp=rc*Idmmax
-Vcpp=Vr-Vrcpp
+rc=20e-3;
+Vrcpp=rc*Idmmax;
+Vcpp=Vr-Vrcpp;
 Cmin=Dmax*Voutnom/(freqnom*Rlmin*Vcpp)
 
 
 % Power losses
 % PMEG3050BEP diode characteristics
-Vf=0.38 % @ 1A
-Pvf=Vf*Ioutmax
+Vf=0.38; % @ 1A
+Pvf=Vf*Ioutmax;
 % conduction power loss (and Vf loss?)
-Pf=0.25 % approx @ 1A
+Pf=0.25; % approx @ 1A
 
 % IRFL4315PBF transistor characteristics
-Qg=12e-9
-Coss % given above
-Rds=185e-3
+Qg=12e-9;
+Rds=185e-3;
 
 % max transistor conduction loss
-Prdsmax=2*Rds*Dmax*Voutnom^2/(3*freqnom*Lp*Rlmin)
+Prdsmax=2*Rds*Dmax*Voutnom^2/(3*freqnom*Lp*Rlmin);
 % switching loss
-Pswmin=freqnom*Coss*Vinmin^2
-Pswmax=freqnom*Coss*Vinmax^2
-Pfetmin=Prdsmax+Pswmin/2
-Pfetmax=Prdsmax+Pswmax/2
+Pswmin=freqnom*Coss*Vinmin^2;
+Pswmax=freqnom*Coss*Vinmax^2;
+Pfetmin=Prdsmax+Pswmin/2;
+Pfetmax=Prdsmax+Pswmax/2;
 
-Prt1max=2*Rp*Dmax*Voutnom^2/(3*freqnom*Lp*Rlmin)
-Prt1=2*Rp*Dmin*Voutnom^2/(3*freqnom*Lp*Rlmin)
-Prt2=2*Nps*Rs*Voutnom^2/(3*Rlmin)*sqrt(2/(freqnom*Lp*Rlmin))
+Prt1max=2*Rp*Dmax*Voutnom^2/(3*freqnom*Lp*Rlmin);
+Prt1=2*Rp*Dmin*Voutnom^2/(3*freqnom*Lp*Rlmin);
+Prt2=2*Nps*Rs*Voutnom^2/(3*Rlmin)*sqrt(2/(freqnom*Lp*Rlmin));
 
 % Current sense resistor
-Vcsmax=0.55
-Rcs=Vcsmax/Ismmax
+Vcsmax=0.55;
+Rcs=Vcsmax/Ismmax;
+% =>  3.1083
+Rcs=3.16
 
-Cize=4.7e-9;
+Cize=1e-9;
 % estimate Rize at 1kHz
 Rize=1/(2*pi*Cize*1e3)
 % Rfbu chosen to be close to Rize=33.9 kOhm
-Rfbu=41.2e3
+%Rfbu=41.2e3
+Rfbu=162e3
 % TLV431 ref voltage
 Vref=1.24;
-Rfbl=Vref*Rfbu/(Voutnom-Vref);
+Rfbl=Vref*Rfbu/(Voutnom-Vref)
 % => 24.8e3, chose to match standard series
-Rfbl=24.3e3
+%Rfbl=24.3e3
+Rfbl=95.3e3
 
 % Op-amp CTR@2ma
-%CTR2ma=0.2
-CTR2ma=0.85
-Vledopto=1.1
-Iledopto=2e-3
-Vledoptok=Vref+150e-3
+CTR=0.85;
+Vledopto=1.1;
+Iledopto=2e-3;
+Vledoptok=Vref+150e-3;
 Rob=(Voutnom-Vledopto-Vledoptok)/Iledopto;
 % => 405, choose Rob=402
 Rob=402
 
 Vzdcmax=1.7;
 Vctlmax=Vzdcmax+2*Vcsmax;
-Vctlnom=(Vzdcmax+Vctlmax)/2
+Vctlnom=(Vzdcmax+Vctlmax)/2;
 Vb=5;
-Rctl=(Vb-Vzdcmax)/(Iledopto*CTR2ma);
+Rctl=(Vb-Vzdcmax)/(Iledopto*CTR)
 % => 1941, choose Rctl=2k
-Rctl=2e3
+Rctl=1.96e3
 
 f=logspace(0,6,500);
 
@@ -146,33 +150,33 @@ f=logspace(0,6,500);
 F0=5.5e3
 
 % TPS23757 internal CTL to PWM divider
-Kctl=2
+Kctl=2;
 
 % current sense resistor voltage slope
-Sn=Vinmin*Rcs/Lp % [V/s]
+Sn=Vinmin*Rcs/Lp; % [V/s]
 % TPS23757 internal slope peak value
-Vslope=155e-3
-Se=Vslope*freqnom/Dmin % [V/s]
+Vslope=155e-3;
+Se=Vslope*freqnom/Dmin; % [V/s]
 % output filter capacitance and ESR
-Cof=10e-6
-ESRCof=10e-3;
+Cof=150e-6
+ESRCof=45e-3;
 
 M=Nps*Voutnom/Vinmin;
 % power stage pole 1
-omegap1=2/(Rlmin*Cof)
+omegap1=2/(Rlmin*Cof);
 fp1=omegap1/(2*pi)
 % power stage pole 2
-omegap2=2*freqnom*(1/(1+1/M))^2
+omegap2=2*freqnom*(1/(1+1/M))^2;
 fp2=omegap2/(2*pi)
 % power stage zero 1
-omegaz1=1/(ESRCof*Cof) % LHPZ
+omegaz1=1/(ESRCof*Cof); % LHPZ
 fz1=omegaz1/(2*pi)
 % power stage zero 2
-omegaz2=Nps^2*Rlmin/(M*(1+M)*Lp)
+omegaz2=Nps^2*Rlmin/(M*(1+M)*Lp);
 fz2=omegaz2/(2*pi)
 
 Gfb=1/Kctl;
-Go=Vinmin*Gfb*sqrt(freqnom*Rlmin/(2*Lp))*1/(Sn+Se)
+Go=Vinmin*Gfb*sqrt(freqnom*Rlmin/(2*Lp))*1/(Sn+Se);
 
 function MPF=MPF(omega, Go, omegaz1, omegaz2, omegap1, omegap2)
     MPF=Go.*(1+i.*omega./omegaz1).*(1-i.*omega./omegaz2) ...
@@ -180,12 +184,12 @@ function MPF=MPF(omega, Go, omegaz1, omegaz2, omegap1, omegap2)
 end
 
 mpfdata=MPF(2*pi.*f, Go, omegaz1, omegaz2, omegap1, omegap2);
-absfigh=figure
+absfigh=figure;
 hold on
 loglog(f, abs(mpfdata), "b;Uncompensated MPF;")
 grid on
 title "Transfer function magnitude"
-argfigh=figure
+argfigh=figure;
 hold on
 semilogx(f, arg(mpfdata), "b;Uncompensated MPF;")
 grid on
@@ -194,31 +198,31 @@ title "Transfer function phase"
 
 % Cctl as parameter since that is the unknown
 % for simplified transfer function OPTOs set Rzctl = 0
-function OPTO=OPTO(omega, Cctl, Rctl, Rzctl, Rob, CTR2ma, Kctl)
-    OPTO=Rctl/Rob*CTR2ma.*(1+i.*omega.*Rzctl.*Cctl)./ ...
+function OPTO=OPTO(omega, Cctl, Rctl, Rzctl, Rob, CTR, Kctl)
+    OPTO=Rctl/Rob*CTR.*(1+i.*omega.*Rzctl.*Cctl)./ ...
         (1+i.*omega.*(Rctl+Rzctl).*Cctl).*1/Kctl;
 end
 
-Rload=Rlmin
+Rload=Rlmin;
 
 % Cctl selected to be sufficiently below 1 at F0. Assuming simple
 % integrator with no midband gain (Riz=0). Target an initial
 % abs(Gmo(2*pi*F0))>0.5-0.8 to estimate Cctl. Cctl must be limited
 % to 47 nF or less. Choose abs(Gmo(2*pi*F0))=0.75
-absGmo=0.75
+absGmotarget=0.75
 %absGmo=1.6
-Cctl=sqrt(1-(Rctl/Rob*CTR2ma/Kctl* ...
-             abs(MPF(2*pi*F0, Go, omegaz1, omegaz2, omegap1, omegap2))/absGmo)^2)/ ...
+Cctl=sqrt(1-(Rctl/Rob*CTR/Kctl* ...
+             abs(MPF(2*pi*F0, Go, omegaz1, omegaz2, omegap1, omegap2))/absGmotarget)^2)/ ...
      (2*pi*F0*Rctl)
 
 Cctl=min(47e-9, Cctl);
 
-decades=[5.6 6.8 8.2 10 12 15 18 22 27 33 39 47].*1e-9;
+decades=[1.0 1.2 1.5 1.8 2.2 2.7 3.3 3.9 4.7 5.6 6.8 8.2 10 12 15 18 22 27 33 39 47].*1e-9;
 [tmp index] = min(abs(decades-Cctl));
 printf("Chosen Cctl within decade boundaries: ")
 Cctl=decades(index)
 
-optodata=OPTO(2*pi.*f, Cctl, Rctl, 0, Rob, CTR2ma, Kctl);
+optodata=OPTO(2*pi.*f, Cctl, Rctl, 0, Rob, CTR, Kctl);
 figure(absfigh)
 loglog(f, abs(optodata), "g;Optocoupler (simple);")
 figure(argfigh)
@@ -228,26 +232,38 @@ semilogx(f, arg(optodata), "g;Optocoupler (simple);")
 % For lower output power and voltage designs, a resistor in series
 % with CCTL can provide phase boost for
 % stability. Target RZCTL ~ RCTL /10).
-Rzctl=249
-%Rzctl=0
+%Rzctl=249
+Rzctl=1780
 
-optodata=OPTO(2*pi.*f, Cctl, Rctl, Rzctl, Rob, CTR2ma, Kctl);
+
+optodata=OPTO(2*pi.*f, Cctl, Rctl, Rzctl, Rob, CTR, Kctl);
 figure(absfigh)
 loglog(f, abs(optodata), "y;Optocoupler (with Rzctl);")
 figure(argfigh)
 semilogx(f, arg(optodata), "y;Optocoupler (with Rzctl);")
 
+
+gmodata=abs(MPF(2*pi*f, Go, omegaz1, omegaz2, omegap1, omegap2)).*...
+        OPTO(2*pi.*f, Cctl, Rctl, Rzctl, Rob, CTR, Kctl);
+figure(absfigh)
+loglog(f, abs(gmodata), "c;Gmo;")
+figure(argfigh)
+semilogx(f, arg(gmodata), "c;Gmo;")
+
 absGmoF0=abs(MPF(2*pi*F0, Go, omegaz1, omegaz2, omegap1, omegap2)) ...
-         *abs(OPTO(2*pi.*F0, Cctl, Rctl, Rzctl, Rob, CTR2ma, Kctl))
+         *abs(OPTO(2*pi.*F0, Cctl, Rctl, Rzctl, Rob, CTR, Kctl))
 
 % overall transfer function unity at selected crossover yields Riz
-% cannot go for unity, will choose 2, makes unstable system??
-Riz=Rfbu*(2/absGmoF0-1)
+Riz=Rfbu*(1/absGmoF0-1)
+Riz=6.81e3
 % choose Ciz for a zero 4-5 times below crossover frequency
-Ciz=5/(2*pi*Riz*F0)
+Ciz=4/(2*pi*Riz*F0)
+% => 1.6997e-08
+Ciz=18e-9
 % Cip for a pole at 10 times F0
-Cip=1/(20*pi*Riz*F0)
-
+Cip=1/(10*pi*Riz*F0)
+%=>  8.4985e-10
+Cip=820e-12
 % integrator stage transfer function
 function INT=INT(omega, Riz, Rfbu, Ciz, Cip)
     INT=Riz/Rfbu.*(1+1./(i.*omega.*Riz.*Ciz))./(1+i.*omega.*Riz.*Cip);
@@ -261,17 +277,19 @@ semilogx(f, arg(intdata), "k;Integrator;")
 
 % overall flyback transfer function
 function FB=FB(omega, Rload, omegaz1, omegaz2, omegap1, omegap2, Cctl, Rctl, Rzctl, Rob, ...
-               CTR2ma, Kctl, Riz, Rfbu, Ciz, Cip)
+               CTR, Kctl, Riz, Rfbu, Ciz, Cip)
     FB=-MPF(omega, Rload, omegaz1, omegaz2, omegap1, omegap2).* ...
-       OPTO(omega, Cctl, Rctl, Rzctl, Rob, CTR2ma, Kctl).* ...
+       OPTO(omega, Cctl, Rctl, Rzctl, Rob, CTR, Kctl).* ...
        (INT(omega, Riz, Rfbu, Ciz, Cip)+1);
 end
 
-fbdata=FB(2*pi.*f, Rload, omegaz1, omegaz2, omegap1, omegap2, Cctl, Rctl, Rzctl, Rob, CTR2ma, ...
+fbdata=FB(2*pi.*f, Rload, omegaz1, omegaz2, omegap1, omegap2, Cctl, Rctl, Rzctl, Rob, CTR, ...
           Kctl, Riz, Rfbu, Ciz, Cip);
 
 
-FBf0=FB(2*pi.*F0, Rload, omegaz1, omegaz2, omegap1, omegap2, Cctl, Rctl, Rzctl, Rob, CTR2ma, ...
+% adjusted F0 = 4.2 kHz
+F0=3.3e3
+FBf0=FB(2*pi.*F0, Rload, omegaz1, omegaz2, omegap1, omegap2, Cctl, Rctl, Rzctl, Rob, CTR, ...
         Kctl, Riz, Rfbu, Ciz, Cip)
 phasemargin=arg(FBf0)*180/pi
 gainF0=20*log10(abs(FBf0))
